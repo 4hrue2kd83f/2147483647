0951e589bcce0f5f05df8b499092882fd6e7ecb8
4hrue2kd83f <ccai2271@gmail.com>
2020-03-11 08:36:48 -0600

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -58,9 +58,9 @@ GameManager.prototype.addStartTiles = function () {
 
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
-  if (this.grid.cellsAvailable() && this.score < 32767) {
-    var value = 1 + Math.floor(this.score / 195);
-    value = Math.min(value, 32767 - this.score);
+  if (this.grid.cellsAvailable() && this.score < 2147483647) {
+    var value = 1 + Math.floor(this.score / 256);
+    value = Math.min(value, 2147483647 - this.score);
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -139,7 +139,7 @@ GameManager.prototype.move = function (direction) {
           tile.updatePosition(positions.next);
 
           // The mighty 2048 tile
-          if (merged.value === 32767) self.won = true;
+          if (merged.value === 2147483647) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
