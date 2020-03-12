685768b53bbca16ec4189617e1c947dcd1557f36
4hrue2kd83f <ccai2271@gmail.com>
2020-03-12 11:46:00 -0600

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -79,7 +79,11 @@ HTMLActuator.prototype.addTile = function (tile) {
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
+
   inner.textContent = tile.value;
+  if (tile.value > 99999999) {
+    inner.textContent = tile.value.toExponential(5);
+  }
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
